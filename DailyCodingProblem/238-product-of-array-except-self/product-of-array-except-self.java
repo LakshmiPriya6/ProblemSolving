9d03c11edc0d85365c044c17fb19f9e23275ed61
LakshmiPriya6 <46064968+LakshmiPriya6@users.noreply.github.com>
2025-05-05 00:10:07 +0530

    Time: 3 ms (21.89%) | Memory: 56.4 MB (16.05%) - LeetSync

--- DailyCodingProblem/238-product-of-array-except-self/product-of-array-except-self.java
+++ DailyCodingProblem/238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,27 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int length = nums.length;
-
-        int[] L = new int[length];
-        int[] R = new int[length];
-
-        int[] answer = new int[length];
-
-        L[0] = 1;
-        for(int i = 1; i< length; i++){
-            L[i] = nums[i-1] * L[i-1];
+        int left = 0, right = nums.length-1;
+        int[] resArr = new int[nums.length];
+        int[] prefixProd =new int[nums.length];
+        int[] sufffixProd =new int[nums.length];
+        prefixProd[0] = 1;
+        for(int i =1; i< nums.length;i++){
+            prefixProd[i] = nums[i-1] * prefixProd[i-1];
+        }
+        sufffixProd[nums.length-1] = 1;
+        for(int i= nums.length-2;i>=0;i--){
+            sufffixProd[i] = nums[i+1] * sufffixProd[i+1];
         }
 
-        R[length -1] = 1;
-        for(int i = length -2; i>=0; i--){
-            R[i] = nums[i+1] * R[i + 1];
+
+        //[1,2,3,4] -> prefixProd [1,2,6,24]
+        for(int i =0; i< nums.length;i++){
+
+            resArr[i] = prefixProd[i] * sufffixProd[i];
         }
-
-        for( int i =0; i < length; i++){
-            answer[i] = L[i] * R[i];
-        }
-
-        return answer;
-
+        return resArr;
     }
 }
