e2f537d3b88f84f621302e08afee993bc467c10e
LakshmiPriya6 <46064968+LakshmiPriya6@users.noreply.github.com>
2025-05-21 13:15:55 +0530

    Time: 1 ms (70.88%) | Memory: 42.5 MB (34.56%) - LeetSync

--- DailyCodingProblem/1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ DailyCodingProblem/1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String gcdOfStrings(String str1, String str2) {
+    /*public String gcdOfStrings(String str1, String str2) {
         int len1 = str1.length();
         int len2 = str2.length();
         for(int i = Math.min(len1, len2); i>=1;i--){
@@ -18,6 +18,23 @@ class Solution {
         } else{
             String base = str1.substring(0,k);
             return str1.replace(base,"").isEmpty() && str2.replace(base,"").isEmpty();
+        }
+    }**/
+
+    public String gcdOfStrings(String str1, String str2){
+        if(!(str1+str2).equals(str2+str1)){
+            return "";
+        }
+
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
+    }
+
+    public int gcd(int len1, int len2){
+        if(len2==0){
+            return len1;
+        } else {
+            return gcd(len2, len1%len2);
         }
     }
 }
