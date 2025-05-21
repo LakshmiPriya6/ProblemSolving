2933a4516ee54d4369451a9cf25bb0afafd2d240
LakshmiPriya6 <46064968+LakshmiPriya6@users.noreply.github.com>
2025-05-21 13:48:15 +0530

    Time: 1 ms (97.49%) | Memory: 46.1 MB (13.88%) - LeetSync

--- DailyCodingProblem/605-can-place-flowers/can-place-flowers.java
+++ DailyCodingProblem/605-can-place-flowers/can-place-flowers.java
@@ -1,19 +1,14 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int count =0;
-        for(int i=0; i < flowerbed.length; i++){
+        int count = 0;
+        for(int i =0; i<flowerbed.length;i++){
+            if(flowerbed[i]==0){
+                boolean emptyLeftPlot = (i==0) || flowerbed[i-1] ==0;
+                boolean emptyRightPlot = (i == flowerbed.length -1) || flowerbed[i+1] ==0;
 
-            if(flowerbed[i] == 0){
-
-                boolean emptyLeft = (i==0) || (flowerbed[i-1] == 0);
-                boolean emptyRight = (i == flowerbed.length-1) || (flowerbed[i+1] == 0);
-
-                if(emptyLeft && emptyRight){
+                if(emptyLeftPlot && emptyRightPlot){
                     flowerbed[i] = 1;
                     count++;
-                    if(count >= n){
-                        break;
-                    }
                 }
             }
         }
