8d4a39d084ee52d7a349bf2dc9b23c2b3612bea9
LakshmiPriya6 <46064968+LakshmiPriya6@users.noreply.github.com>
2025-05-21 15:53:05 +0530

    Time: 2 ms (88.80%) | Memory: 55.8 MB (36.80%) - LeetSync

--- DailyCodingProblem/238-product-of-array-except-self/product-of-array-except-self.java
+++ DailyCodingProblem/238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,24 +1,19 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int left = 0, right = nums.length-1;
-        int[] resArr = new int[nums.length];
-        int[] prefixProd =new int[nums.length];
-        int[] sufffixProd =new int[nums.length];
+        int[] prefixProd = new int[nums.length];
+        int[] suffixProd = new int[nums.length];
+        int[] result = new int[nums.length];
         prefixProd[0] = 1;
-        for(int i =1; i< nums.length;i++){
-            prefixProd[i] = nums[i-1] * prefixProd[i-1];
+        for(int i = 1; i <nums.length; i++){
+           prefixProd[i] = nums[i-1] * prefixProd[i-1];
         }
-        sufffixProd[nums.length-1] = 1;
-        for(int i= nums.length-2;i>=0;i--){
-            sufffixProd[i] = nums[i+1] * sufffixProd[i+1];
+        suffixProd[nums.length-1]=1;
+        for(int i = nums.length-2; i >=0; i--){
+           suffixProd[i] = nums[i+1] * suffixProd[i+1];
         }
-
-
-        //[1,2,3,4] -> prefixProd [1,2,6,24]
-        for(int i =0; i< nums.length;i++){
-
-            resArr[i] = prefixProd[i] * sufffixProd[i];
+        for(int i = 0; i <nums.length; i++){
+            result[i] = prefixProd[i] * suffixProd[i];
         }
-        return resArr;
+        return result;
     }
 }
