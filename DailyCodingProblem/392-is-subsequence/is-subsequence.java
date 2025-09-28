7bdc399ce8df6313b6c270ba92c96b51f7cfb88b
LakshmiPriya6 <46064968+LakshmiPriya6@users.noreply.github.com>
2025-09-28 23:19:08 +0530

    Time: 2 ms (66.89%) | Memory: 41.6 MB (38.78%) - LeetSync

--- DailyCodingProblem/392-is-subsequence/is-subsequence.java
+++ DailyCodingProblem/392-is-subsequence/is-subsequence.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        Integer p1 = 0;
-        Integer p2 = 0;
-        Integer leftBound = s.length();
-        Integer rightBound = t.length();
+        // Use primitive int for pointers to avoid boxing/unboxing overhead.
+        int p1 = 0;
+        int p2 = 0;
         
-            while(p1<s.length() && p2 < t.length()){
-                
-                    if(s.charAt(p1) == t.charAt(p2)){
-                        p1 += 1;
-                    } 
-                        p2 +=1;
-                }
-                return p1 == s.length();
-        }    
+        while (p1 < s.length() && p2 < t.length()) {
+            // If characters match, advance the pointer for s.
+            if (s.charAt(p1) == t.charAt(p2)) {
+                p1++;
+            }
+            // Always advance the pointer for t to scan through it.
+            p2++;
+        }
+        
+        // If p1 has traversed the entire length of s, it's a subsequence.
+        return p1 == s.length();
+    }
 }
