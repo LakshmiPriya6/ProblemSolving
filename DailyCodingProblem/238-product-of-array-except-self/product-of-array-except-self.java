9a30508c3a01ccdbada47a173fa572529949e817
LakshmiPriya6 <46064968+LakshmiPriya6@users.noreply.github.com>
2025-09-28 23:51:57 +0530

    Time: 2 ms (87.25%) | Memory: 56.2 MB (23.23%) - LeetSync

--- DailyCodingProblem/238-product-of-array-except-self/product-of-array-except-self.java
+++ DailyCodingProblem/238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] prefixProd = new int[nums.length];
-        int[] suffixProd = new int[nums.length];
+        int[] leftProd = new int[nums.length];
+        int[] rightProd = new int[nums.length];
         int[] result = new int[nums.length];
-        prefixProd[0] = 1;
-        for(int i = 1; i <nums.length; i++){
-           prefixProd[i] = nums[i-1] * prefixProd[i-1];
+
+        leftProd[0] = 1;
+        for(int i=1;i<nums.length;i++){
+            leftProd[i] = leftProd[i-1] * nums[i-1];
         }
-        suffixProd[nums.length-1]=1;
-        for(int i = nums.length-2; i >=0; i--){
-           suffixProd[i] = nums[i+1] * suffixProd[i+1];
+        rightProd[nums.length-1] = 1;
+        for(int i=nums.length-2; i>=0;i--){
+            rightProd[i] = rightProd[i+1] * nums[i+1];
         }
-        for(int i = 0; i <nums.length; i++){
-            result[i] = prefixProd[i] * suffixProd[i];
+        for(int i=0;i<nums.length;i++){
+            result[i] = rightProd[i] * leftProd[i];
         }
         return result;
     }
