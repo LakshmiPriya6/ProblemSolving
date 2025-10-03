d4a0484060f650be38a4604e71c8a41ac4d7d3ab
LakshmiPriya6 <46064968+LakshmiPriya6@users.noreply.github.com>
2025-10-03 15:29:46 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (69.28%) - LeetSync

--- DailyCodingProblem/142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ DailyCodingProblem/142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -1,35 +1,21 @@
-import java.util.HashSet;
-import java.util.Set;
+class Solution {
+  public ListNode detectCycle(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
 
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        if (head == null) {
-            return null;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast) {
+        slow = head;
+        while (slow != fast) {
+          slow = slow.next;
+          fast = fast.next;
         }
-    
-        Set<ListNode> seenNodes = new HashSet<>();
-        ListNode curr = head;
-        while (curr != null) {
-         
-            if (seenNodes.contains(curr)) {
-              return curr;
-            } else {
-                seenNodes.add(curr);
-                curr = curr.next;
-            }
-        }
-   
-        return null;
+        return slow;
+      }
     }
+
+    return null;
+  }
 }
