e6dd573e96107fcde555983523cc289ac3ae90a0
LakshmiPriya6 <46064968+LakshmiPriya6@users.noreply.github.com>
2025-10-04 16:49:01 +0530

    Time: 3 ms (96.51%) | Memory: 44.6 MB (22.93%) - LeetSync

---
+++ DailyCodingProblem/437-path-sum-iii/path-sum-iii.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    int k;
+    HashMap<Long, Integer> h = new HashMap();
+
+    public void preorder(TreeNode node, long currSum){
+        if(node == null){
+            return;
+        }
+
+        currSum += node.val;
+
+        if(currSum == k){
+            count++;
+        }
+
+        count+= h.getOrDefault(currSum - k, 0);
+        h.put(currSum, h.getOrDefault(currSum, 0)+1);
+
+        preorder(node.left, currSum);
+        preorder(node.right, currSum);
+
+        h.put(currSum, h.get(currSum) -1);
+;    }
+
+    public int pathSum(TreeNode root, int targetSum) {
+        k = targetSum;
+        preorder(root, 0L);
+        return count;
+    }
+}
