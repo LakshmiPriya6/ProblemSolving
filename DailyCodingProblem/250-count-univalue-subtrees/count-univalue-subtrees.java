e236dc329a23c66fcaad94a8cca0b63c9ca7b3ba
LakshmiPriya6 <46064968+LakshmiPriya6@users.noreply.github.com>
2025-10-04 13:30:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (59.39%) - LeetSync

--- DailyCodingProblem/250-count-univalue-subtrees/count-univalue-subtrees.java
+++ DailyCodingProblem/250-count-univalue-subtrees/count-univalue-subtrees.java
@@ -1,45 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    private Pair<Boolean, Integer> dfs(TreeNode node){
-        if(node == null){
-            return new Pair<>(true,0);
+    private boolean dfs(TreeNode root, int[] count) {
+        if (root == null) {
+            return true;
         }
 
-        Pair<Boolean, Integer> leftPair = dfs(node.left);
-        Pair<Boolean, Integer> rightPair = dfs(node.right);
-        boolean left = leftPair.getKey();
-        boolean right = rightPair.getKey();
-        int count = leftPair.getValue() + rightPair.getValue();
+        boolean isLeftUnival = dfs(root.left, count);
+        boolean isRightUnival = dfs(root.right, count);
 
-        if(left && right){
-            if(node.left !=null && node.left.val != node.val){
-                return new Pair<>(false,count);
-            }
-            if(node.right !=null && node.right.val != node.val){
-                return new Pair<>(false,count);
-            }
-            count++;
-            return new Pair<>(true,count);
+        if (isLeftUnival && isRightUnival &&
+            (root.left == null || root.left.val == root.val) &&
+            (root.right == null || root.right.val == root.val)
+        ) {
+            count[0]++;
+            return true;
         }
-        return new Pair<>(false,count);
+
+        return false;
     }
+    
     public int countUnivalSubtrees(TreeNode root) {
-        return dfs(root).getValue();
+        int[] count = new int[1];
+        dfs(root, count);
+        return count[0];
     }
-
-
 }
