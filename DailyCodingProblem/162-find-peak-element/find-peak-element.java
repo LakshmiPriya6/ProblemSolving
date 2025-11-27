734fa262cd25dfd7853be3f59e107bce0142739f
LakshmiPriya6 <46064968+LakshmiPriya6@users.noreply.github.com>
2025-11-27 21:11:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (35.85%) - LeetSync

--- DailyCodingProblem/162-find-peak-element/find-peak-element.java
+++ DailyCodingProblem/162-find-peak-element/find-peak-element.java
@@ -7,7 +7,6 @@ class Solution {
         if (l==r) return l;
         int mid = l + (r-l)/2;
         if(nums[mid]>nums[mid+1]) return search(nums, l, mid);
-        return search(nums, mid+1, r);
-        //return -1;
+        else return search(nums, mid+1, r);
     }
 }
