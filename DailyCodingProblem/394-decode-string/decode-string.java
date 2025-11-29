3178baba2c2fcd1e1f0e21610c412fc8cffb9315
LakshmiPriya6 <46064968+LakshmiPriya6@users.noreply.github.com>
2025-11-30 01:27:25 +0530

    Time: 3 ms (19.64%) | Memory: 43.1 MB (8.46%) - LeetSync

--- DailyCodingProblem/394-decode-string/decode-string.java
+++ DailyCodingProblem/394-decode-string/decode-string.java
@@ -1,39 +1,32 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Character> stack= new Stack<>();
-
-        for(int i= 0; i< s.length();i++){
-            if(s.charAt(i) == ']'){
-                List<Character> decodedString = new ArrayList<>();
-
+        Stack<Character> stack = new Stack();
+        for(char c : s.toCharArray()){
+            if(c != ']'){
+                stack.push(c);
+            } else{
+                StringBuilder decodedString = new StringBuilder();
                 while(stack.peek() != '['){
-                    decodedString.add(stack.pop());
+                    decodedString.append(stack.pop());
                 }
-
-                stack.pop();
-
+                stack.pop(); //'['
                 int base = 1;
                 int k = 0;
-
                 while(!stack.isEmpty() && Character.isDigit(stack.peek())){
-                    k = k + (stack.pop() - '0') * base;
+                    k = k + (stack.pop()-'0') * base;
                     base*=10;
                 }
-
+                decodedString.reverse();
                 while(k!=0){
-                    for(int j=decodedString.size()-1;j>=0;j--){
-                        stack.push(decodedString.get(j));
+                    for(int j=0; j<decodedString.length(); j++){ // FIX: Loop from start to end
+                        stack.push(decodedString.charAt(j));
                     }
                     k--;
                 }
             }
-
-            else{
-                stack.push(s.charAt(i));
-            }
         }
         char[] result = new char[stack.size()];
-        for(int i = result.length -1; i>=0; i--){
+        for (int i = result.length - 1; i >= 0; i--) {
             result[i] = stack.pop();
         }
         return new String(result);
