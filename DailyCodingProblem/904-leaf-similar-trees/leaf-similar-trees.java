7dd51818a79afd143d0c3a00a1821b4bb4dcabc9
LakshmiPriya6 <46064968+LakshmiPriya6@users.noreply.github.com>
2025-11-29 16:15:30 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (12.07%) - LeetSync

--- DailyCodingProblem/904-leaf-similar-trees/leaf-similar-trees.java
+++ DailyCodingProblem/904-leaf-similar-trees/leaf-similar-trees.java
@@ -15,24 +15,23 @@
  */
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        ArrayList<Integer> leaves1 = new ArrayList<>();
-        ArrayList<Integer> leaves2 = new ArrayList<>();
+        ArrayList<Integer> leaves1 = new ArrayList<Integer>();
+        ArrayList<Integer> leaves2 = new ArrayList<Integer>();
         leaves1 = getLeafNodes(root1, leaves1);
         leaves2 = getLeafNodes(root2, leaves2);
         return leaves1.equals(leaves2);
-
     }
 
-    private ArrayList<Integer> getLeafNodes(TreeNode root,ArrayList<Integer> leaves){
+    private ArrayList<Integer> getLeafNodes(TreeNode root, ArrayList<Integer> leaves){
         if(root == null){
             return leaves;
         }
-        if(root!=null){
+        else{
             if(root.left == null && root.right == null){
                 leaves.add(root.val);
             } else {
-                getLeafNodes(root.left, leaves);
-                getLeafNodes(root.right, leaves);
+                getLeafNodes(root.left,leaves);
+                getLeafNodes(root.right,leaves);
             }
         }
         return leaves;
