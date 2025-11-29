401954fc5cbcd3fef7cb09a18be72381455d13c8
LakshmiPriya6 <46064968+LakshmiPriya6@users.noreply.github.com>
2025-11-29 18:47:44 +0530

    Time: 3 ms (96.63%) | Memory: 46.4 MB (15.13%) - LeetSync

--- DailyCodingProblem/437-path-sum-iii/path-sum-iii.java
+++ DailyCodingProblem/437-path-sum-iii/path-sum-iii.java
@@ -14,10 +14,11 @@
  * }
  */
 class Solution {
+
     int count = 0;
     int k;
     HashMap<Long, Integer> h = new HashMap();
-
+    
     public void preorder(TreeNode node, long currSum){
         if(node == null){
             return;
@@ -28,15 +29,16 @@ class Solution {
         if(currSum == k){
             count++;
         }
-
-        count+= h.getOrDefault(currSum - k, 0);
+        
+        count += h.getOrDefault(currSum - k, 0);
+        
         h.put(currSum, h.getOrDefault(currSum, 0)+1);
 
         preorder(node.left, currSum);
         preorder(node.right, currSum);
 
-        h.put(currSum, h.get(currSum) -1);
-;    }
+        h.put(currSum, h.get(currSum)-1);
+    }
 
     public int pathSum(TreeNode root, int targetSum) {
         k = targetSum;
