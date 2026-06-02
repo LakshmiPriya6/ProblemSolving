0c3f8e201a16f088cb5647d1c0d0f31a144b6503
LakshmiPriya6 <46064968+LakshmiPriya6@users.noreply.github.com>
2026-06-02 16:08:11 +0530

    Time: 77 ms (5.08%), Space: 43.5 MB (72.68%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,41 @@
+class Solution {
+    // public String gcdOfStrings(String str1, String str2) {
+    //     int len1 = str1.length();
+    //     int len2 = str2.length();
+    //     for(int i= Math.min(len1,len2); i>=1;--i){
+    //         if(valid(str1, str2,i)){
+    //             return str1.substring(0,i);
+    //         }
+    //     }
+    //     return "";
+    // }
+
+    // private boolean valid(String str1, String str2, int k){
+    //     int len1 = str1.length();
+    //     int len2 = str2.length();
+    //     if(len1%k>0 ||len2%k >0){
+    //         return false;
+    //     } else{
+    //         String base = str1.substring(0,k);
+    //         return str1.replace(base,"").isEmpty() && str2.replace(base,"").isEmpty();
+    //     }
+    // }
+    private int gcd(int x, int y){
+        if(y==0){
+            return x;
+        } else {
+            return gcd(y, x%y);
+        }
+    }
+
+    public String gcdOfStrings(String str1, String str2){
+       int len1 = str1.length();
+       int len2 = str2.length();
+       if(!(str1+str2).equals(str2+str1)){
+          return "";
+       } else{
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
+       }
+    }
+}
